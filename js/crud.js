508822ebcfae881e4612c1fb666b814d5de053d2
duluong <luongvandu@gmail.com>
2017-09-22 22:38:41 +0900

    'update'

--- js/crud.js
+++ js/crud.js
@@ -1,9 +1,5 @@
-<script language = "javascript" type = "text/javascript">
-
 $("#productList").on('click','tr',function(e){
     e.preventDefault();
     var id = $(this).attr('value');
     alert(id);
 }); 
-
-</script>
