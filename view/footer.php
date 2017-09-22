88e66845bb4602d362c6a64a1fe91c2b0a20427a
Onsiter <Onsiter>
2017-09-22 11:39:52 +0900

    'u'

--- view/footer.php
+++ view/footer.php
@@ -4,5 +4,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
  
+    <p >
+    	copyright dulv
+    </p>
 </body>
 </html>
